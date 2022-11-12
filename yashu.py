4d36690ad29b5099113907dbd1a016f046785b87
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-12 12:04:39 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -65,13 +65,13 @@ async def kang(u: Update, c: CallbackContext):
     except:
         if not title:
             return await m.reply_text("/hkang [emoji] [packnum] [packname]")
-        await m.reply_text("your new pack is created with name" + " " + title)
         if format == "video":
             await c.bot.create_new_sticker_set(user_id=user.id, name=pack_name, title=title, emojis=emoji, webm_sticker=input("lmao.webm"))
         elif format == "animated":
             await c.bot.create_new_sticker_set(user_id=user.id, name=pack_name, title=title, emojis=emoji, tgs_sticker=open(x, "rb"))
         else:
             await c.bot.create_new_sticker_set(user_id=user.id, name=pack_name, title=title, emojis=emoji, png_sticker=sticid)
+        await m.reply_text("your new pack is created with name" + " " + title)
         edited_keyboard = InlineKeyboardMarkup(
                     [
                         [
