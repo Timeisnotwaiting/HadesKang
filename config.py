98771e7e8a9b946574caaadc7540826c96f16cc4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-06 11:22:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ as e
 
-BOT_TOKEN = "5774044322:AAEZPO_qmiwl17rm7y-83bIfk4wQwCNkvD8"
+BOT_TOKEN = "5774044322:AAEMmHzN0YSTef2wQB_nHcNAuu_NvvFHJgY"
 """
 SUDOS = e["SUDO_USERS"].split()
 
