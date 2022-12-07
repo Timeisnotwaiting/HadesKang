2381480084bfc63e08fff8ee3d8a21a3e0ff9db2
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-07 10:04:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from os import environ as e
 
-BOT_TOKEN = "5774044322:AAEMmHzN0YSTef2wQB_nHcNAuu_NvvFHJgY"
-"""
+BOT_TOKEN = e["BOT_TOKEN"]
+
 SUDOS = e["SUDO_USERS"].split()
 
 x = []
 
 for y in SUDOS:
     x.append(int(y))
-"""
-SUDO_USERS = [5711561310, 5868832590]
+
+SUDO_USERS = x
